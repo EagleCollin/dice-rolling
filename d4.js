6724a69e9ce0b1de1e3fe76cd957f6c6d1889195
EagleCollin <scoutcollin97@gmail.com>
2022-03-19 10:10:37 -0500

    Streamlining

--- d4.js
+++ d4.js
@@ -12,13 +12,14 @@ function rollDice(e, sideDie){
     e.preventDefault();
     let rolled = []
     for (let i=0; i<numberDice.value; i++){
-        let dice = Math.floor(Math.random() * sideDie) + 1
-        rolled.push(dice)
-        console.error()
+        let dice = Math.floor(Math.random() * sideDie) + 1;
+        rolled.push(dice);
+        console.error();
     }
 
-    diceTotal.innerText = `${rolled}`
-    console.log(rolled)
+    diceTotal.innerText = `${rolled}`;
+    console.log(rolled);
+    addDice(rolled);
     return rolled;
     
 }
@@ -33,47 +34,15 @@ function updateQuantityLabel() {
 function addDice(e){
     if (e.target.match(".btn-20")) return;
 
-    
+    rolled.reduce((a,b) => ())
 }
-
-//roll different dice
-function rollD4(e){
-    if (!e.target.matches(".btn-d4")) return;
-
-    rollDice(e, 4);
-}
-
-function rollD8(e){
-    if (!e.target.matches(".btn-d8")) return;
-
-    rollDice(e, 8);
-}
-
-function rollD10(e){
-    if (!e.target.matches(".btn-d10")) return;
-
-    rollDice(e, 10);
-}
-
-function rollD12(e){
-    if (!e.target.matches(".btn-d12")) return;
-
-    rollDice(e, 12);
-}
-
-function rollD20(e){
-    if (!e.target.matches(".btn-d20")) return;
-
-    rollDice(e, 20);
-}
-
 
 //event listeners: when dice is selected
-d4.addEventListener("submit", (e)=>rollD4(e));
-d8.addEventListener("submit", (e)=>rollD8(e));
-d10.addEventListener("submit", (e)=>rollD10(e))
-d12.addEventListener("submit", (e)=>rollD12(e))
-d20.addEventListener("submit", (e)=>rollD20(e))
+d4.addEventListener("click", (e)=>rollDice(e, 4));
+d8.addEventListener("click", (e)=>rollD8(e, 8));
+d10.addEventListener("click", (e)=>rollD10(e, 10))
+d12.addEventListener("click", (e)=>rollD12(e, 10))
+d20.addEventListener("click", (e)=>rollD20(e, 20))
 
 //event listener: when hits enter for quantity
 numberDice.addEventListener("input", updateQuantityLabel);
